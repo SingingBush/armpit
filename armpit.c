0ee6331bd66feae0933433f79c4a259de24d1e7c
Samael Bate <singingbush@hotmail.com>
2013-02-02 15:09:59 +0000

    checking op code for data processing

--- armpit.c
+++ armpit.c
@@ -3,39 +3,6 @@
  * 			gcc armpit.c -std=c99 -o armpit
  */
 
-/*
- * Condition Codes:
- *
-	AL – Always				NV – Never
-	EQ – Equal				NE – Not Equal
-	VS – Overflow Set		VC – Overflow Clear
-	MI – Minus				PL – Plus
-	CS – Carry Set			CC – Carry Clear
-	HI – Higher				LS – Lower or Same
-	GE – Greater or equal	LT – Less Than
-	GT – Greater than		LE – Less or Equal
- *
- */
-#define COND_CODE_POS (28)
-
-#define CONDITION_EQUAL	(0x0) // 0000
-#define CONDITION_NOT_EQUAL	(0x1) // 0001
-#define CONDITION_CARRY_SET	(0x2) // 0010
-#define CONDITION_CARRY_CLEAR	(0x3) // 0011
-#define CONDITION_MINUS	(0x4) // 0100
-#define CONDITION_PLUS	(0x5) // 0101
-#define CONDITION_OVERFLOW_SET	(0x6) // 0110
-#define CONDITION_OVERFLOW_CLEAR	(0x7) // 0111
-#define CONDITION_HIGHER	(0x8) // 1000
-#define CONDITION_LOWER_OR_SAME	(0x9) // 1001
-#define CONDITION_GREATER_OR_EQUAL	(0xa) // 1010
-#define CONDITION_LESS_THAN	(0xb) // 1011
-#define CONDITION_GREATER_THAN	(0xc) // 1100
-#define CONDITION_LESS_OR_EQUAL	(0xd) // 1101
-#define CONDITION_ALWAYS	(0xe) // 1110
-#define CONDITION_NEVER	(0xf) // 1111
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -45,7 +12,6 @@
 //#include <unistd.h>	// can either use unistd.h or getopt.h for handling args.
 
 #include "armpit.h"     // contains some defines and my function prototypes.
-
 
 
 // registers:
@@ -88,79 +54,6 @@ REGISTER PROGRAM_COUNTER;   // R15 is used as program counter
 //                  i.e. memory is implicitly accessed on a word aligned boundary.)
 
 
-// now define some bitmasks:
-#define BITMASK_1_BIT 0X1
-#define BITMASK_2_BIT 0X3
-#define BITMASK_3_BIT 0X7
-#define BITMASK_4_BIT 0Xf
-#define BITMASK_5_BIT 0X1f
-#define BITMASK_6_BIT 0X3f
-#define BITMASK_7_BIT 0X7f
-#define BITMASK_8_BIT 0Xff
-
-
-/* -------------------------------------------------------------------------------
- * 	 OpCode (21..24) - Identifies the specific Data Processing Instruction
- * -------------------------------------------------------------------------------
- */
-#define OP_AND	(0x0) // 0000
-#define OP_EOR	(0x1) // 0001
-#define OP_SUB	(0x2) // 0010
-#define OP_RSB	(0x3) // 0011
-#define OP_ADD	(0x4) // 0100
-#define OP_ADC	(0x5) // 0101
-#define OP_SBC	(0x6) // 0110
-#define OP_RSC	(0x7) // 0111
-#define OP_TST	(0x8) // 1000
-#define OP_TEQ	(0x9) // 1001
-#define OP_CMP	(0xa) // 1010
-#define OP_CMN	(0xb) // 1011
-#define OP_ORR	(0xc) // 1100
-#define OP_MOV	(0xd) // 1101
-#define OP_BIC	(0xe) // 1110
-#define OP_MVN	(0xf) // 1111
-
-
-// STATUS_REGISTER
-
-/*
- * Status Register - 8 bits
- *
- * -----------------------------------
- * | 7 | 6 | 5 | 4 | 3 | 2 | 1  | 0  |
- * -----------------------------------
- * | N | Z | C | V | I | F | S1 | S0 |
- * -----------------------------------
- *
- * Bits 7-4 Status flag that identify the result of the last operation.
- *
- * Bit 7  Negative flag
- * Bit 6  Zero flag
- * Bit 5  Carry flag
- * Bit 4  Overflow flag
- *
- * Bits 3-2 Interrupt disable flags, when set prevent the specified type of interrupt from being serviced
- *
- * Bit 3  Interrupt disable flag
- * bit 2  Fast interrupt disable flag
- *
- * Bits 1-0 Identify the processor operation mode (User mode is 0 0)
- *
- * bit 1   Processor mode bit
- * bit 0   Processor mode bit
-*/
-BYTE STATUS_REGISTER;
-
-#define STATUS_N	(1<<7) // negative
-#define STATUS_Z	(1<<6) // zero flag
-#define STATUS_C	(1<<5) // carry flag
-#define STATUS_V	(1<<4) // overflow flag
-#define STATUS_I	(1<<3) // interrupt disable flag
-#define STATUS_I	(1<<3) // Interrupt disable flag
-#define STATUS_F	(1<<2) // Fast interrupt disable flag
-#define STATUS_S1	(1<<1) // processor mode
-#define STATUS_S0	(1<<0) // processor mode
-
 
 /*
  * Global Variables:
@@ -176,25 +69,23 @@ INSTRUCTION memory[1024]; // 4 kilobytes of RAM in this example (since 1 instruc
 
 
 
-/*	------------------------------------------------------------------------
-		Function Prototypes now in armpit.h
-	----------------------------------------------------------------------*/
-
-
-
 // function from: stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
-const char *byte_to_binary(int x) {
-    static char b[9];
-    b[0] = '\0';
+// const char *byte_to_binary(int x, int max, const int bits) {
+//     static int size = bits+1;
+//     static char b[size];
+//     b[0] = '\0';
+    
+//     if(!(max > 1)) {
+//         max = 128;
+//     }
 
-    int z;
-    for (z = 128; z > 0; z >>= 1) {
-        strcat(b, ((x & z) == z) ? "1" : "0");
-    }
+//     for (max > 0; max >>= 1) {
+//         strcat(b, ((x & max) == max) ? "1" : "0");
+//     }
 
-    x,z=0;
-    return b;
-}
+//     x,max=0;
+//     return b;
+// }
 
 /*
  * should potentially take some arguments. I want to use '-f' to point to a text file of 
@@ -228,9 +119,6 @@ int main(int argc, char *argv[]) {
                 displayUsage();
                 exit(EXIT_FAILURE);
                 break;
-            default:
-                /* You won't actually get here. */
-                break;
         }
         opt = getopt( argc, argv, optString );
     }
@@ -254,7 +142,7 @@ int main(int argc, char *argv[]) {
 
             switch(type) {
                 case ALU:
-                    doDataProcessing();
+                    doDataProcessing(instruction);
                     break;
                 case BRANCH:
                     doBranch();
@@ -272,6 +160,8 @@ int main(int argc, char *argv[]) {
                 printf("\tInstruction %i skipped!\n", counter);
             }
         }
+        printf("\nPress Enter key to continue\n");
+        getchar();
     }   
 
     printf("\nExiting program\n\n");
@@ -335,10 +225,19 @@ INSTRUCTION fetchInstruction() {
 InstructionType getInstructionType(INSTRUCTION instruction) {
     InstructionType type;
 
-    type = (instruction.data32 >> 26) & BITMASK_3_BIT;
+    type = (instruction.data32 >> 26) & BITMASK_2_BIT;
 
     if(VERBOSE) {
-        printf("\tInstruction is type: %d\n", type);
+        char *typeName;
+
+        switch(type) {
+            case ALU: typeName = "ALU"; break;
+            case BRANCH: typeName = "Branch"; break;
+            case DATA_TRANSFER: typeName = "Data Transfer"; break;
+            case INTERUPT: typeName = "Interupt"; break;
+        }
+    
+        printf("\tInstruction is type %d:\t%s\n", type, typeName);
     }
     return type; // returns 0, 1, 2, or 3 for ALU, BRANCH, DATA_TRANSFER, or INTERUPT
 }
@@ -459,6 +358,12 @@ bool shouldExecute(INSTRUCTION instruction) {
     return execute; 
 }
 
+bool isImmediateValue(INSTRUCTION instruction) {
+    bool immediate = (instruction.data32 >> 25) & BITMASK_1_BIT;
+    printf("\tImmediate Bit:\t%s \n", immediate? "set" : "not set");
+    return immediate;
+}
+
 int isSet(int flag) {
     return (STATUS_REGISTER & flag);
 }
@@ -475,9 +380,66 @@ void clearFlag(int flag) {
     STATUS_REGISTER = STATUS_REGISTER & (~flag);
 }
 
-void doDataProcessing() {
-    printf("\tData Processing\n");
+void doDataProcessing(INSTRUCTION instruction) {
+    printf("\tData Processing...\n");
     // todo
+    bool immediate = isImmediateValue(instruction);
+
+    int opCode = (instruction.data32 >> OP_CODE_POS) & BITMASK_4_BIT;
+
+    printf("\tOp Code:\t%i\n", opCode);
+
+    switch (opCode) {
+        case OP_AND: // 0000
+            // doAND();
+            printf("\tProcessing AND function\n");
+            break;
+        case OP_EOR: // 0001
+            printf("\tProcessing Exclusive-OR function\n");
+            break;
+        case OP_SUB: // 0010
+            printf("\tProcessing SUB function\n");
+            break;
+        case OP_RSB: // 0011
+            printf("\tProcessing RSB function\n");
+            break;
+        case OP_ADD: // 0100
+            printf("\tProcessing ADD function\n");
+            break;
+        case OP_ADC: // 0101
+            printf("\tProcessing ADC function\n");
+            break;
+        case OP_SBC: // 0110
+            printf("\tProcessing SBC function\n");
+            break;
+        case OP_RSC: // 0111
+            printf("\tProcessing RSC function\n");
+            break;
+        case OP_TST: // 1000
+            printf("\tProcessing TST function\n");
+            break;
+        case OP_TEQ: // 1001
+            printf("\tProcessing TEQ function\n");
+            break;
+        case OP_CMP: // 1010
+            printf("\tProcessing Compare function\n");
+            break;
+        case OP_CMN: // 1011
+            printf("\tProcessing CMN function\n");
+            break;
+        case OP_ORR: // 1100
+            printf("\tProcessing ORR function\n");
+            break;
+        case OP_MOV: // 1101
+            printf("\tProcessing MOV function\n");
+            break;
+        case OP_BIC: // 1110
+            printf("\tProcessing BIC function\n");
+            break;
+        case OP_MVN: // 1111
+            printf("\tProcessing MVN function\n");
+            break;
+    }
 }
 
 void doBranch() {
@@ -494,3 +456,31 @@ void doInterupt() {
     printf("\tInterupt\n");
     // todo
 }
+
+
+
+
+/*
+ * Adds op1Value to op2Value and stores in the specified register
+ *
+ * enter:   regNumber - the number of the destination register (0-15)
+ *          op1Value  - the first value to be added
+ *          op2Value  -  the second value to be added
+ *          setSR     - flag, if non-zero then the status register should be updated, else it shouldn't
+ */
+// void doAdd(int regNumber, REGISTER op1Value, REGISTER op2Value, bool setSR) {
+//     REGISTER result = op1Value + op2Value;
+
+//     registers[ regNumber ] = result;
+
+//     if ( setSR ) {
+//         // set the SR flags by examining the result. This can be difficult for certain flags!
+
+//         // some are simple, e.g. to set or clear the Zero flag, could use -
+//         if ( result == 0 ) {
+//             setFlag(STAT_Z);
+//         } else {
+//             clearFlag(STAT_Z);
+//         }
+//     }
+// }
--- armpit.h
+++ armpit.h
@@ -19,6 +19,112 @@ typedef enum InstructionType {
 typedef uint32_t REGISTER;
 
 
+#define BITMASK_1_BIT 0X1
+#define BITMASK_2_BIT 0X3
+#define BITMASK_3_BIT 0X7
+#define BITMASK_4_BIT 0Xf
+#define BITMASK_5_BIT 0X1f
+#define BITMASK_6_BIT 0X3f
+#define BITMASK_7_BIT 0X7f
+#define BITMASK_8_BIT 0Xff
+
+
+/*
+ * Condition Codes:
+ *
+	AL – Always				NV – Never
+	EQ – Equal				NE – Not Equal
+	VS – Overflow Set		VC – Overflow Clear
+	MI – Minus				PL – Plus
+	CS – Carry Set			CC – Carry Clear
+	HI – Higher				LS – Lower or Same
+	GE – Greater or equal	LT – Less Than
+	GT – Greater than		LE – Less or Equal
+ *
+ */
+#define COND_CODE_POS (28)
+
+#define CONDITION_EQUAL	(0x0) // 0000
+#define CONDITION_NOT_EQUAL	(0x1) // 0001
+#define CONDITION_CARRY_SET	(0x2) // 0010
+#define CONDITION_CARRY_CLEAR	(0x3) // 0011
+#define CONDITION_MINUS	(0x4) // 0100
+#define CONDITION_PLUS	(0x5) // 0101
+#define CONDITION_OVERFLOW_SET	(0x6) // 0110
+#define CONDITION_OVERFLOW_CLEAR	(0x7) // 0111
+#define CONDITION_HIGHER	(0x8) // 1000
+#define CONDITION_LOWER_OR_SAME	(0x9) // 1001
+#define CONDITION_GREATER_OR_EQUAL	(0xa) // 1010
+#define CONDITION_LESS_THAN	(0xb) // 1011
+#define CONDITION_GREATER_THAN	(0xc) // 1100
+#define CONDITION_LESS_OR_EQUAL	(0xd) // 1101
+#define CONDITION_ALWAYS	(0xe) // 1110
+#define CONDITION_NEVER	(0xf) // 1111
+
+
+/*
+ * Status Register - 8 bits
+ *
+ * -----------------------------------
+ * | 7 | 6 | 5 | 4 | 3 | 2 | 1  | 0  |
+ * -----------------------------------
+ * | N | Z | C | V | I | F | S1 | S0 |
+ * -----------------------------------
+ *
+ * Bits 7-4 Status flag that identify the result of the last operation.
+ *
+ * Bit 7  Negative flag
+ * Bit 6  Zero flag
+ * Bit 5  Carry flag
+ * Bit 4  Overflow flag
+ *
+ * Bits 3-2 Interrupt disable flags, when set prevent the specified type of interrupt from being serviced
+ *
+ * Bit 3  Interrupt disable flag
+ * bit 2  Fast interrupt disable flag
+ *
+ * Bits 1-0 Identify the processor operation mode (User mode is 0 0)
+ *
+ * bit 1   Processor mode bit
+ * bit 0   Processor mode bit
+*/
+BYTE STATUS_REGISTER;
+
+#define STATUS_N	(1<<7) // negative
+#define STATUS_Z	(1<<6) // zero flag
+#define STATUS_C	(1<<5) // carry flag
+#define STATUS_V	(1<<4) // overflow flag
+#define STATUS_I	(1<<3) // interrupt disable flag
+#define STATUS_I	(1<<3) // Interrupt disable flag
+#define STATUS_F	(1<<2) // Fast interrupt disable flag
+#define STATUS_S1	(1<<1) // processor mode
+#define STATUS_S0	(1<<0) // processor mode
+
+
+/* -------------------------------------------------------------------------------
+ * 	 OpCode (21..24) - Identifies the specific Data Processing Instruction
+ * -------------------------------------------------------------------------------
+ */
+#define OP_CODE_POS (21)
+
+#define OP_AND	(0x0) // 0000
+#define OP_EOR	(0x1) // 0001
+#define OP_SUB	(0x2) // 0010
+#define OP_RSB	(0x3) // 0011
+#define OP_ADD	(0x4) // 0100
+#define OP_ADC	(0x5) // 0101
+#define OP_SBC	(0x6) // 0110
+#define OP_RSC	(0x7) // 0111
+#define OP_TST	(0x8) // 1000
+#define OP_TEQ	(0x9) // 1001
+#define OP_CMP	(0xa) // 1010
+#define OP_CMN	(0xb) // 1011
+#define OP_ORR	(0xc) // 1100
+#define OP_MOV	(0xd) // 1101
+#define OP_BIC	(0xe) // 1110
+#define OP_MVN	(0xf) // 1111
+
+
 
 /*  ------------------------------------------------------------------------
  *		Function Prototypes
@@ -38,6 +144,8 @@ void doDataProcessing();
 void doBranch();
 void doDataTransfer();
 void doInterupt();
+
+bool isImmediateValue();
 
 int isSet(int);
 int isClear(int);
